da59af65d37183a29a804cd33d757c6a91dde5c4
MOVZX <movzx@yahoo.com>
2026-02-28 21:53:16 +0700

    onyx-kernel: Add scripts to decompile and recompile DTBs
    These are simple helper scripts that must be executed from within
    the images/dtbs directory.
    
    * images/dtbs/decompile.sh: Helper to disassemble dtbo/dtb to dts
    * images/dtbs/recompile.sh: Helper to compile dts back to dtbo/dtb
    
    Signed-off-by: MOVZX <movzx@yahoo.com>

---
+++ images/dtbs/decompile.sh
@@ -0,0 +1,5 @@
+#!/bin/env bash
+
+dtc -I dtb -O dts -o 01_dtbdump_Qualcomm_Technologies,_Inc._Tuna_SoC.dts "01_dtbdump_Qualcomm_Technologies,_Inc._Tuna_SoC.dtb"
+
+dtc -I dtb -O dts -o 02_dtbdump_Qualcomm_Technologies,_Inc._Tuna_7_SoC.dts "02_dtbdump_Qualcomm_Technologies,_Inc._Tuna_7_SoC.dtb"
---
+++ images/dtbs/recompile.sh
@@ -0,0 +1,5 @@
+#!/bin/env bash
+
+dtc -I dts -O dtb -o "01_dtbdump_Qualcomm_Technologies,_Inc._Tuna_SoC.dtb" 01_dtbdump_Qualcomm_Technologies,_Inc._Tuna_SoC.dts
+
+dtc -I dts -O dtb -o "02_dtbdump_Qualcomm_Technologies,_Inc._Tuna_7_SoC.dtb" 02_dtbdump_Qualcomm_Technologies,_Inc._Tuna_7_SoC.dts
